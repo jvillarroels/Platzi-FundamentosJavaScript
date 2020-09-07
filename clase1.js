12427ead3645ab92b2aa71353f824aa702ef7eb6
Jose Villarroel <josedelvallevs@gmail.com>
2020-09-06 23:08:06 -0500

    Lección 3: Variables

---
+++ clase1.js
@@ -0,0 +1,8 @@
+//console.log('Hola Mundo JS!')
+var nombre = 'Sacha',  apellido = 'Lifszyc'
+var edad = '28 años'
+//nombre = 'Sacha'
+console.log('Hola ' + nombre + ' ' + apellido) 
+console.log('Tengo ' + edad)
+
+var peso = '75 Kgs'
