e4757988f0aa756a785908cc58d16978c9ef4beb
Jose Villarroel <josedelvallevs@gmail.com>
2020-09-07 08:34:01 -0500

    Lección 5: Variables: Números

---
+++ clase3.js
@@ -0,0 +1,29 @@
+var edad = 27
+
+//edad = edad + 1
+edad += 1
+
+var peso = 75 
+//peso = peso - 2
+peso -= 2
+
+var sandwich = 1
+//peso = peso + sandwich
+peso += sandwich
+
+var juagarAlFutbol = 3
+peso -= juagarAlFutbol
+
+var precioDeVino = 200.3
+//var total = precioDeVino * 3
+//var total = precioDeVino * 100 * 3 /100
+var total = Math.round(precioDeVino * 100 * 3) /100
+
+var totalStr = total.toFixed(3)
+var total2 = parseFloat(totalStr)
+
+var pizza = 8
+var persona = 2
+var cantidadDePorcionesPorPersona = pizza / persona
+
+
