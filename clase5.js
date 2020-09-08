6f729cfdbf6a27c80204a89767e9c1f4e5826c5e
Jose Villarroel <josedelvallevs@gmail.com>
2020-09-08 07:33:17 -0500

    Leccción 7: El alcance de las funciones

---
+++ clase5.js
@@ -0,0 +1,18 @@
+var nombre = 'Sacha'
+
+
+function imprimirNombreEnMayusculas(nombre) {
+    nombre = nombre.toUpperCase()
+    console.log(nombre)
+}
+/*
+function imprimirNombreEnMayusculas(n) {
+    n = n.toUpperCase()
+    console.log(`Nombre en Mayusculas ${n}`)
+}
+*/
+
+imprimirNombreEnMayusculas(nombre)
+
+console.log(`Nombre original ${nombre}`)
+//console.log(`Variable local de la funcion - n ${n}`)
