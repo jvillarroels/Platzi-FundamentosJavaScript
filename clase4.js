6ce60ba6a7a378b0b5f57fa425094b4f482e7a01
Jose Villarroel <josedelvallevs@gmail.com>
2020-09-08 00:04:58 -0500

    Lección 6: Funciones

---
+++ clase4.js
@@ -0,0 +1,15 @@
+var nombre = 'Sacha', edad = 29
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`)
+}
+    
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Eric', 24)
+imprimirEdad('Dario', 27)
+imprimirEdad(25, 'Carlos')
+imprimirEdad('Juan')
+
+
+
