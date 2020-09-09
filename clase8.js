ac69f59f29b7f5cb14db86abb19b4ba2a3f8c2c5
Jose Villarroel <josedelvallevs@gmail.com>
2020-09-08 23:42:12 -0500

    Lección 10: Parámetros como referencia o como valor

---
+++ clase8.js
@@ -0,0 +1,29 @@
+var sacha = {
+    nombre: 'Sacha',
+    apellido: 'Lifszyc',
+    edad: 28
+}
+
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Susnisky',
+    edad: 27
+}
+
+function imprimirNombreEnMayusculas(persona) {
+    // vara nombre = persona.nombre
+    var { nombre } = persona
+    console.log(nombre.toUpperCase())
+}
+
+imprimirNombreEnMayusculas(sacha)
+imprimirNombreEnMayusculas(dario)
+
+function cumpleanos(persona) {
+    return {
+        ...persona, 
+        edad: persona.edad + 1
+    }
+}
+
+
